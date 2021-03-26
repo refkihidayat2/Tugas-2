28877e01e1f9ba5468fd606804428d7ab068a794
refkihidayat2 <refkihidayat29@gmail.com>
2021-03-26 23:58:40 +0700

    Tugas-2

--- Soal-1.py
+++ Soal-1.py
@@ -1,3 +1,7 @@
+
+
+
+
 def menu():
     print ("Selamat datang!")
     print ("---MENU---")
@@ -5,26 +9,43 @@ def menu():
     print ("2, Tambah Kontak")
     print ("3, Keluar")
 
+nama = []
+notelpon = []
 
 def Tambah():
-    A = str(input("Masukkan nama :"))
-    B = int(input("Masukkan No Telepon :"))
-    print ("Nama :", A)
-    print ("No Telepon :", B)
+    while True:
+        A = str(input("Masukkan nama :"))
+        B = int(input("Masukkan No Telepon :"))
+        nama.append(A)
+        notelpon.append(B)
+        break
 
 
 menu()
 pil = int(input("Masukkan pilihan : "))
 while pil != 0:
     if pil == 1:
+        for a in nama:
+            print("Nama:", a)
+        for b in notelpon:
+            print("No telpon:", b)
+        continue
+
+    elif pil == 2:
         Tambah()
-    
+        print ("Kontak berhasil ditambahkan")
+
     elif pil == 3:
         print("Program selesai, sampai jumpa!")
-    
+        break
+
+    else:
+        print("Menu tidak tersedia")
+
     print()
     menu()
     pil = int(input("Masukkan pilihan : "))
+
 
 
 
---
+++ s.py
@@ -0,0 +1,5 @@
+mylist = [1, 2, 3, 4, 5, 6]
+mylist.append('refki')
+mylist.append('andre')
+for x in mylist:
+    print(x)
